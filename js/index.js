cc2a8b382dc48e5eb7e6e3ec1fa8402c683580a5
rengifolu <andrelujan7@gmail.com>
2019-04-13 13:15:54 +0200

    actualizacion 13-04-19

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var titulo = null;
-var menu = null;
 
 window.onload = function inic() {
+
   // PARA RESPONSIVE DE NAV
   var x = window.matchMedia("(max-width: 414px)");
   myFunction(x); // Call listener function at run time
@@ -17,15 +17,14 @@ window.onload = function inic() {
 };
 
 function primero() {
-  menu = document.getElementById("menu");
-  menu.style.display = "none";
+
   titulo = document.getElementById("titulo");
   titulo.style.fontSize = "15em";
   titulo.style.transitionDuration = "2s";
   titulo.style.opacity = 1;
 }
 
-function openMenu() {
+/*  function openMenu() {
   cajaTitulo = document.getElementById("cajaTitulo");
   cajaTitulo.style.display = "none";
   menu.style.display = "block";
@@ -33,12 +32,10 @@ function openMenu() {
   menu.style.fontSize = "5em";
   menu.style.transitionDuration = "5s";
   menu.style.marginLeft = "50%";
-}
+} */
 
-function changeClass() {
-  document.getElementById("open-hide").classList.toggle("show");
-}
 
+/*
 window.onload = function() {
   document.getElementById("open-hide").addEventListener("click", changeClass);
-};
+}; */
